4ae70fbb0796d26ae67c7f842e6bf44e412614fb
jin park <jinpark0625@gmail.com>
2022-05-15 21:45:34 -0400

    [ADD]added image carousel

--- src/components/imageBox.jsx
+++ src/components/imageBox.jsx
@@ -6,51 +6,59 @@ import "@egjs/react-flicking/dist/flicking-inline.css";
 import Panel from "./panel";
 import { Parallax } from "@egjs/flicking-plugins";
 import "../styles/slide.css";
+import styled from "styled-components";
 
-// const ImageBoxStyle = {
-//   position: "relative",
-//   paddingTop: "75%",
-//   overflow: "hidden",
-// };
+export const CardImageWrap = styled.div`
+  position: relative;
+  padding-top: 75%;
+  overflow: hidden;
+`;
+
+export const CardImage = styled.img`
+  top: -100%;
+  bottom: -100%;
+  margin: auto;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%);
+  width: auto;
+  height: 100%;
+`;
+
+export const CardSlideWrap = styled(Flicking)`
+  width: 100%;
+  height: 100%;
+  margin: 0px auto;
+  background: #fff;
+  background: rgba(55, 55, 55 0.1);
+  border-radius: 5px;
+`;
 
 const ImageBox = () => {
   const plugins = [new Parallax("img", 4)];
 
   return (
-    <Flicking
-      className="flicking"
-      //   align="prev"
+    <CardSlideWrap
       gap={10}
       circular={true}
       onMoveEnd={(e) => {
         console.log(e);
       }}
-      //   autoResize={true}
       plugins={plugins}
-      //   style={ImageBoxStyle}
     >
-      {/* <Panel index={1} />
-      <Panel index={2} />
-      <Panel index={3} /> */}
-      <div className="panel">
-        <img
-          src="https://naver.github.io/egjs-flicking/images/bg01.jpg"
-          alt="store_img"
-        />
-      </div>
-      <div className="panel">
-        <img
-          src="https://naver.github.io/egjs-flicking/images/bg02.jpg"
-          alt="store_img"
-        />
-      </div>
-      <div className="panel">
-        <img
-          src="https://naver.github.io/egjs-flicking/images/bg03.jpg"
-          alt="store_img"
-        />
-      </div>
-    </Flicking>
+      <Panel
+        index={1}
+        src="https://naver.github.io/egjs-flicking/images/bg01.jpg"
+      />
+      <Panel
+        index={2}
+        src="https://naver.github.io/egjs-flicking/images/bg02.jpg"
+      />
+      <Panel
+        index={3}
+        src="https://naver.github.io/egjs-flicking/images/bg03.jpg"
+      />
+    </CardSlideWrap>
   );
 };
 
--- src/components/panel.jsx
+++ src/components/panel.jsx
@@ -1,21 +1,40 @@
 import React from "react";
 import styled from "styled-components";
 
-// export const Panel = styled.div`
-//   position: absolute;
-//   top: 0;
-//   left: 0;
-//   right: 0;
-//   bottom: 0;
-//   max-width: 100%;
-//   height: auto;
-// `;
+export const CardImageWrap = styled.div`
+  position: relative;
+  padding-top: 75%;
+  overflow: hidden;
+  display: block;
+  border-radius: 16px;
+  background: #fff;
+  text-align: center;
+  width: 100%;
 
-export default React.forwardRef(({ index }, ref) => (
-  <div ref={ref}>
-    <img
-      src={require("../assets/images/default_img.png")}
-      style={{ width: "300px", height: "300px" }}
-    />
-  </div>
+  &:after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background: rgba(0, 0, 0, 0.1);
+  }
+`;
+
+export const CardImage = styled.img`
+  top: -100%;
+  bottom: -100%;
+  margin: auto;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%);
+  width: auto;
+  height: 100%;
+`;
+
+export default React.forwardRef(({ index, src }, ref) => (
+  <CardImageWrap ref={ref}>
+    <CardImage src={src} alt="store_img" />
+  </CardImageWrap>
 ));
